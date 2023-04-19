35a51d174443ffb800a08636e7401a3f9898986a
Darwin Gualotuña <daosgc@gmail.com>
2023-04-19 10:38:04 -0500

    run all tests

--- src/app/ecomerce/components/item-card/item-card.component.spec.ts
+++ src/app/ecomerce/components/item-card/item-card.component.spec.ts
@@ -3,7 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ItemCardComponent } from './item-card.component';
 import { IItem, IItemCart } from '../../interfaces/ecommer.interface';
 
-fdescribe('ItemCardComponent', () => {
+describe('ItemCardComponent', () => {
   let component: ItemCardComponent;
   let fixture: ComponentFixture<ItemCardComponent>;
 
